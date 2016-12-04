fb97a9fa376cb89dfb6c5dcf25db8a9f77dcbfcc
Petar Kekez <petar.kekez@outlook.com>
2016-12-04 17:43:15 +0100

    Added use of static files and indexhtml with login form

--- src/AspMvcCoreWorkshop/Startup.cs
+++ src/AspMvcCoreWorkshop/Startup.cs
@@ -28,10 +28,8 @@ namespace AspMvcCoreWorkshop
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
